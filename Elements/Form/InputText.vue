b8ae278c5140c380afd20ab35927dccc4e4fd8e0
anloc <1876294+anloc@users.noreply.github.com>
2021-05-18 13:08:44 +0300

    Create InputText.vue

---
+++ Elements/Form/InputText.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form-group">
+    <label :for="labelId"><slot></slot> <span v-if="$attrs.required === true">*</span></label>
+    <input v-bind="$attrs" type="text"
+           class="form-control"
+           :id="labelId"
+           :value="modelValue"
+           @input="$emit('update:modelValue', $event.target.value)">
+  </div>
+</template>
+
+<script>
+
+import Utils from "../../Mixins/Utils";
+import {uuid} from "../../Mixins/Utilsv3";
+import {computed} from 'vue';
+
+export default {
+  mixins: [
+    Utils
+  ],
+  props: {
+    modelValue : {
+      require: false
+    }
+  },
+  emits: ['update:modelValue'],
+  setup() {
+    const labelId = computed(() => "label_" + uuid());
+
+    return {
+      labelId
+    }
+  }
+}
+</script>
