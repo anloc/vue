31cc5a60cd3b2e13c4db2f56e42b4c726dd68d30
anloc <1876294+anloc@users.noreply.github.com>
2021-05-18 13:25:12 +0300

    Create DeleteIcon.vue

---
+++ Elements/Icons/DeleteIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <Icon name="x" style="color: red;" />
+</template>
+
+<script>
+
+import Icon from "./Icon";
+
+export default {
+  name: 'DeleteIcon',
+  components: {
+    Icon
+  }
+}
+</script>
